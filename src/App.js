95bcb1cffc2105c1e12f85bd8a02b208ce62fcc0
Aurimas <aurimas@darguzis.com>
2019-06-24 18:26:14 +0100

    getting on with the Router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
