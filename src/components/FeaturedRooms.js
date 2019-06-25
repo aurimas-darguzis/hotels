144eedb528b0ebf8816cb1744564bfd2778bf0ce
Aurimas <aurimas@darguzis.com>
2019-06-25 12:45:24 +0100

    added spiiner

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { RoomContext } from '../context';
+import Loading from './Loading';
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+
+  render() {
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    return (
+      <div>
+        Featured Rooms
+        <Loading />;
+      </div>
+    );
+  }
+}
