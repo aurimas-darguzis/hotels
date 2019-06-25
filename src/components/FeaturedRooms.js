dfd19fd5fa1ccbaa6610df32d45f0eb2cecea0fc
Aurimas <aurimas@darguzis.com>
2019-06-25 12:46:20 +0100

    component: Room

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -11,6 +12,7 @@ export default class FeaturedRooms extends Component {
     return (
       <div>
         Featured Rooms
+        <Room />
         <Loading />;
       </div>
     );
