4e4037c185e2bcc3e93943274067282d0012cc92
Aurimas <aurimas@darguzis.com>
2019-06-26 15:53:51 +0100

    component: RoomContainer and friends

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import RoomFilter from './RoomFilter'
+import RoomList from './RoomList'
+
+
+export default function RoomContainer() {
+  return (
+    <div>
+      Rooms Container
+      <RoomFilter />
+      <RoomList />
+    </div>
+  )
+}
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import RoomContainer from '../components/RoomContainer';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 
 export default function Rooms() {
   return (
-    <Hero hero='roomsHero'>
-      <Banner title='our rooms'>
-        <Link to='/' className='btn-primary'>
-          return home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero='roomsHero'>
+        <Banner title='our rooms'>
+          <Link to='/' className='btn-primary'>
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomContainer />
+    </>
   );
 }
